35f6224de5f15404dadee058d8d161402284a88b
curiositypaths <curiositypaths@users.noreply.github.com>
2017-08-16 12:22:42 -0400

    Cleanup solution

--- src/components/pokemonLister.js
+++ src/components/pokemonLister.js
@@ -9,31 +9,53 @@ class PokemonLister {
   initBindingsAndEventListiners(containerID) {
     this.pokemonsNodeContainer = document.getElementById('pokemon-container')
     this.searchForm = document.getElementById('pokemon-seach-input')
-    this.searchForm.addEventListener('keyup',this.findPokemons.bind(this))
-    this.pokemonsNodeContainer.addEventListener('click',this.flipPokemonCard.bind(this))
-
+    this.searchForm.addEventListener('keyup',this.displaySearchResults.bind(this))
+    this.pokemonsNodeContainer.addEventListener('click',this.handlePokemonCardTogle.bind(this))
   }
 
   parsePokemonsJSON(pokemonsJSON) {
     return pokemonsJSON.map( pokemonJSON => new Pokemon(pokemonJSON) )
   }
 
-  findPokemons() {
+  displaySearchResults() {
     this.restoreFrontImages()
-    if (this.searchForm.value !== '') {
-      this.pokemonsToDisplay = this.pokemons.filter( pokemon => pokemon.name.includes(this.searchForm.value) )
-    } else {
-      this.pokemonsToDisplay = []
-    }
+    this.searchPokemons()
     this.render()
   }
 
-  flipPokemonCard() {
+  searchPokemons() {
+    this.validSearchTerm() ? this.displayOnlyReleventPokemons() : this.hideAllPokemons()
+  }
+
+  validSearchTerm() {
+    return this.searchForm.value !== ''
+  }
+
+  displayOnlyReleventPokemons() {
+    this.pokemonsToDisplay = this.pokemons.filter( pokemon => pokemon.name.includes(this.searchForm.value) )
+  }
+
+  hideAllPokemons() {
+    this.pokemonsToDisplay = []
+  }
+
+  findClickedPokemon(event) {
+    return this.pokemons.find( pokemon => pokemon.name === event.target.dataset.pokename)
+  }
+
+  handlePokemonCardTogle() {
     if (event.target.dataset.action === 'flip-image') {
-        const pokemon = this.pokemons.find( pokemon => pokemon.name === event.target.dataset.pokename)
-        pokemon.image === pokemon.frontImage ? pokemon.image = pokemon.backImage : pokemon.image = pokemon.frontImage
-        this.render()
+      this.findPokemonCardAndTogle(event)
+      this.render()
     }
+  }
+
+  findPokemonCardAndTogle(event) {
+    this.toglePokemonCard(this.findClickedPokemon(event))
+  }
+
+  toglePokemonCard(pokemon) {
+    pokemon.image === pokemon.frontImage ? pokemon.image = pokemon.backImage : pokemon.image = pokemon.frontImage
   }
 
   restoreFrontImages() {
